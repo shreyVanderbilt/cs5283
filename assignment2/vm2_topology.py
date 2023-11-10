4b573e5fa873dcc89a2c4a34966c61adfe0bf8b1
Nelson Torres <nelsontorresjr330@gmail.com>
2023-11-10 16:48:16 -0600

    testing changes

--- assignment2/vm2_topology.py
+++ assignment2/vm2_topology.py
@@ -5,8 +5,7 @@ from mininet.node import Node, OVSSwitch, Controller
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink
-from mininet.topo import SingleSwitchTopo
-from mininet.clean import cleanup
+from mininet.topo import Topo
 
 class LinuxRouter(Node):
     "A Node with IP forwarding enabled."
@@ -21,47 +20,55 @@ class LinuxRouter(Node):
         self.cmd('sysctl net.ipv4.ip_forward=0')
         super(LinuxRouter, self).terminate()
 
+class SingleSwitchTopo(Topo):
+    "Single switch connected to n hosts."
+    def build(self, n=2):
+        switch = self.addSwitch('s1')
+        for h in range(n):
+            # Each host gets 50%/n of system CPU
+            host = self.addHost('h%s' % (h + 1),
+                                cpu=.5/n)
+            # 10 Mbps, 5ms delay, 2% loss, 1000 packet queue
+            self.addLink(host, switch, bw=10, delay='5ms', loss=2, max_queue_size=1000, use_htb=True)
+
 def createTopology():
     "Create the network."
-    
-    # Create SingleSwitchTopo instances for each LAN
-    lan_p_topo = SingleSwitchTopo(2)  # Two hosts in LAN P
-    lan_u_topo = SingleSwitchTopo(2)  # Two hosts in LAN U
-
-    # Create a Mininet instance for each LAN
-    net_p = Mininet(topo=lan_p_topo, controller=Controller, switch=OVSSwitch, link=TCLink)
-    net_u = Mininet(topo=lan_u_topo, controller=Controller, switch=OVSSwitch, link=TCLink)
-
-    # Start the network for LAN P and U
-    net_p.start()
-    net_u.start()
-
-    # Note: You will need to merge the two Mininet instances or design your network to be within a single Mininet instance.
-    # This example shows how to start two separate Mininet instances, which is not typical usage.
-    # You will need to interconnect these LANs through routers or switches manually.
+    net = Mininet(controller=Controller, switch=OVSSwitch, link=TCLink)
 
     # Add controller
-    c0 = net_p.addController('c0')
+    c0 = net.addController('c0')
 
-    # Create routers with multiple IP addresses
-    rP = net_p.addHost('rP', cls=LinuxRouter, ip='172.16.3.1/24')
-    rU = net_u.addHost('rU', cls=LinuxRouter, ip='10.85.10.1/24')
+    # Create and add LANs to the network
+    lan_p_topo = SingleSwitchTopo(2)  # Two hosts in LAN P
+    lan_u_topo = SingleSwitchTopo(2)  # Two hosts in LAN U
+    lan_p = lan_p_topo.build()
+    lan_u = lan_u_topo.build()
 
-    # Configure IP addresses for routers
-    rP.cmd('ifconfig rP-eth0 172.16.3.1/24')
-    rP.cmd('ifconfig rP-eth0:0 172.16.5.1/24')  # Virtual interface for the second IP
-    rU.cmd('ifconfig rU-eth0 10.85.10.1/24')
-    rU.cmd('ifconfig rU-eth0:0 10.85.8.1/24')   # Virtual interface for the second IP
+    # Create routers
+    rP = net.addHost('rP', cls=LinuxRouter, ip='172.16.3.1/24')
+    rU = net.addHost('rU', cls=LinuxRouter, ip='10.85.10.1/24')
 
-    # Assuming you have switches or routers to interconnect LAN P and LAN U
-    # You will need to create those and connect them appropriately here.
+    # Connect the routers to the LANs
+    # Use canonical switch names
+    sP = net.addSwitch('s1')
+    sU = net.addSwitch('s2')
+    net.addLink(sP, rP, intfName2='rP-eth1', params2={'ip': '172.16.3.1/24'})
+    net.addLink(sU, rU, intfName2='rU-eth1', params2={'ip': '10.85.10.1/24'})
 
-    CLI(net_p)
-    CLI(net_u)
-    net_p.stop()
-    net_u.stop()
+    # Add a second interface to routers P and U for the second subnet
+    net.addLink(sP, rP, intfName2='rP-eth2', params2={'ip': '172.16.5.1/24'})
+    net.addLink(sU, rU, intfName2='rU-eth2', params2={'ip': '10.85.8.1/24'})
+
+    # Start the network
+    net.start()
+
+    # Configure routes on routers P and U for their second subnet
+    rP.cmd('ip route add 172.16.5.0/24 dev rP-eth2')
+    rU.cmd('ip route add 10.85.8.0/24 dev rU-eth2')
+
+    CLI(net)
+    net.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')
-    cleanup()
     createTopology()
