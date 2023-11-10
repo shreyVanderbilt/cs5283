8271aa4f90bc369d5502fa39bcb9bc9c916a4377
Nelson Torres <nelsontorresjr330@gmail.com>
2023-11-10 16:12:13 -0600

    New topo test

--- assignment2/vm2_topology.py
+++ assignment2/vm2_topology.py
@@ -22,41 +22,43 @@ class LinuxRouter(Node):
 
 def createTopology():
     "Create the network."
-    net = Mininet(controller=Controller, switch=OVSSwitch, link=TCLink)
-
-    # Add controller
-    c0 = net.addController('c0')
-
+    
     # Create SingleSwitchTopo instances for each LAN
     lan_p_topo = SingleSwitchTopo(2)  # Two hosts in LAN P
     lan_u_topo = SingleSwitchTopo(2)  # Two hosts in LAN U
 
-    # Create routers
-    rP = net.addHost('rP', cls=LinuxRouter, ip='172.16.3.1/24')
-    rU = net.addHost('rU', cls=LinuxRouter, ip='10.85.10.1/24')
+    # Create a Mininet instance for each LAN
+    net_p = Mininet(topo=lan_p_topo, controller=Controller, switch=OVSSwitch, link=TCLink)
+    net_u = Mininet(topo=lan_u_topo, controller=Controller, switch=OVSSwitch, link=TCLink)
 
-    # Create and add LANs to the network
-    lan_p = net.addTopo(lan_p_topo)
-    lan_u = net.addTopo(lan_u_topo)
+    # Start the network for LAN P and U
+    net_p.start()
+    net_u.start()
 
-    # Connect the routers to the LANs
-    for lan, router, ip in [(lan_p, rP, '172.16.3.1/24'), (lan_u, rU, '10.85.10.1/24')]:
-        switch = next(iter(lan.switches()))  # Get the single switch from the LAN
-        net.addLink(switch, router, intfName2='%s-eth1' % router.name, params2={'ip': ip})
+    # Note: You will need to merge the two Mininet instances or design your network to be within a single Mininet instance.
+    # This example shows how to start two separate Mininet instances, which is not typical usage.
+    # You will need to interconnect these LANs through routers or switches manually.
 
-    # Add a second interface to routers P and U for the second subnet
-    net.addLink(net.get('sP'), rP, intfName2='rP-eth2', params2={'ip': '172.16.5.1/24'})
-    net.addLink(net.get('sU'), rU, intfName2='rU-eth2', params2={'ip': '10.85.8.1/24'})
+    # Add controller
+    c0 = net_p.addController('c0')
 
-    # Start the network
-    net.start()
+    # Create routers with multiple IP addresses
+    rP = net_p.addHost('rP', cls=LinuxRouter, ip='172.16.3.1/24')
+    rU = net_u.addHost('rU', cls=LinuxRouter, ip='10.85.10.1/24')
 
-    # Configure routes on routers P and U for their second subnet
-    rP.cmd('ip route add 172.16.5.0/24 dev rP-eth2')
-    rU.cmd('ip route add 10.85.8.0/24 dev rU-eth2')
+    # Configure IP addresses for routers
+    rP.cmd('ifconfig rP-eth0 172.16.3.1/24')
+    rP.cmd('ifconfig rP-eth0:0 172.16.5.1/24')  # Virtual interface for the second IP
+    rU.cmd('ifconfig rU-eth0 10.85.10.1/24')
+    rU.cmd('ifconfig rU-eth0:0 10.85.8.1/24')   # Virtual interface for the second IP
 
-    CLI(net)
-    net.stop()
+    # Assuming you have switches or routers to interconnect LAN P and LAN U
+    # You will need to create those and connect them appropriately here.
+
+    CLI(net_p)
+    CLI(net_u)
+    net_p.stop()
+    net_u.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')
