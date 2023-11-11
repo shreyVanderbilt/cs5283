215ef62abe512d3b0daec6a3b528a2f8490fb740
Nelson Torres <nelsontorresjr330@gmail.com>
2023-11-10 19:52:05 -0600

    more tests

--- assignment2/vm1_topology.py
+++ assignment2/vm1_topology.py
@@ -1,53 +1,47 @@
-from mininet.topo import SingleSwitchTopo
+from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import OVSSwitch, Controller
 from mininet.log import setLogLevel
 from mininet.cli import CLI
 
-class UniDirectionalTopo(SingleSwitchTopo):
+class UniDirectionalTopo(Topo):
     def build(self):
-        # Create two single switch topologies
-        # First single switch topology with two hosts
-        SingleSwitchTopo.build(self, n=2)
-        # Rename hosts and switch for the first topology
-        self.hosts[0].name = 'h1'
-        self.hosts[1].name = 'h2'
-        self.switches[0].name = 's1'
-        
-        # Second single switch topology with two hosts
-        SingleSwitchTopo.build(self, n=2)
-        # Rename hosts and switch for the second topology
-        self.hosts[2].name = 'h3'
-        self.hosts[3].name = 'h4'
-        self.switches[1].name = 's2'
-        
+        # Add switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+
+        # Add hosts to the first switch with specific IPs
+        h1 = self.addHost('h1', ip='192.168.1.1/24')
+        h2 = self.addHost('h2', ip='192.168.1.2/24')
+        # Add hosts to the second switch with specific IPs
+        h3 = self.addHost('h3', ip='192.168.2.1/24')
+        h4 = self.addHost('h4', ip='192.168.2.2/24')
+
+        # Add links between hosts and their respective switches
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s2)
+        self.addLink(h4, s2)
+
         # Connect the two switches
-        self.addLink('s1', 's2')
+        self.addLink(s1, s2)
 
 def setupNetwork():
-    "Create network and run simple performance test"
+    "Create a network with unidirectional flow from s1 to s2"
     topo = UniDirectionalTopo()
     net = Mininet(topo=topo, switch=OVSSwitch, controller=Controller)
     net.start()
 
-    # Assign specific IP addresses to the hosts
-    net.get('h1').setIP('192.168.1.1', 24)
-    net.get('h2').setIP('192.168.1.2', 24)
-    net.get('h3').setIP('192.168.2.1', 24)
-    net.get('h4').setIP('192.168.2.2', 24)
-
-    # Setup unidirectional flow from s1 to s2
+    # Setup unidirectional flow from s1 to s2 by adding OpenFlow rules to drop packets from s2 to s1
     s2 = net.get('s2')
-    # Block all incoming traffic to s1 (192.168.1.0/24)
-    s2.cmd('ovs-ofctl add-flow s2 priority=65535,ip,nw_dst=192.168.1.0/24,actions=drop')
-    # Allow all other traffic to pass through normally
+    s2.cmd('ovs-ofctl add-flow s2 priority=65535,ip,nw_src=192.168.2.0/24,nw_dst=192.168.1.0/24,actions=drop')
     s2.cmd('ovs-ofctl add-flow s2 priority=0,actions=normal')
 
     print("Dumping host connections")
     dumpNodeConnections(net.hosts)
     print("Testing network connectivity")
     net.pingAll()
-    CLI(net)  # This line is optional, it starts a CLI for interactive debugging
+    CLI(net)  # Optionally start a CLI for interactive debugging
     net.stop()
 
 if __name__ == '__main__':
