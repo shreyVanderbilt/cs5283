07016720607f01e39f800f3769df8d94e947f531
Nelson Torres <nelsontorresjr330@gmail.com>
2023-11-10 20:20:42 -0600

    more tests

--- assignment2/vm1_topology.py
+++ assignment2/vm1_topology.py
@@ -1,54 +1,44 @@
-from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import OVSSwitch, Controller
-from mininet.log import setLogLevel
+from mininet.node import Controller, OVSSwitch
 from mininet.cli import CLI
-from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.topo import Topo
 
-class UniDirectionalTopo(Topo):
+class CustomTopo(Topo):
     def build(self):
-        # Add switches
+        # First switch with two hosts
         s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-
-        # Add hosts to the first switch with specific IPs
         h1 = self.addHost('h1', ip='192.168.1.1/24')
         h2 = self.addHost('h2', ip='192.168.1.2/24')
-        # Add hosts to the second switch with specific IPs
-        h3 = self.addHost('h3', ip='192.168.2.1/24')
-        h4 = self.addHost('h4', ip='192.168.2.2/24')
-
-        # Add links between hosts and their respective switches
         self.addLink(h1, s1)
         self.addLink(h2, s1)
+
+        # Second switch with two hosts
+        s2 = self.addSwitch('s2')
+        h3 = self.addHost('h3', ip='192.168.2.1/24')
+        h4 = self.addHost('h4', ip='192.168.2.2/24')
         self.addLink(h3, s2)
         self.addLink(h4, s2)
 
         # Connect the two switches
         self.addLink(s1, s2)
 
-def setupNetwork():
-    "Create a network with unidirectional flow from s1 to s2"
-    topo = UniDirectionalTopo()
-    net = Mininet(topo=topo, switch=OVSSwitch, controller=Controller)
+def create_network():
+    # Initialize Mininet with the custom topology
+    net = Mininet(topo=CustomTopo(), controller=Controller, switch=OVSSwitch)
+
+    # Start the network
     net.start()
 
-    # Adding static routes to hosts on s1 for subnet on s2
-    net['h1'].cmd('route add -net 192.168.2.0 netmask 255.255.255.0 dev h1-eth0')
-    net['h2'].cmd('route add -net 192.168.2.0 netmask 255.255.255.0 dev h2-eth0')
-
-    # Adding static routes to hosts on s2 for subnet on s1
-    # This is only needed if you want bidirectional communication
-    # net['h3'].cmd('route add -net 192.168.1.0 netmask 255.255.255.0 dev h3-eth0')
-    # net['h4'].cmd('route add -net 192.168.1.0 netmask 255.255.255.0 dev h4-eth0')
-
-    print("Dumping host connections")
-    dumpNodeConnections(net.hosts)
-    print("Testing network connectivity")
+    # Test network connectivity
     net.pingAll()
-    CLI(net)  # Optionally start a CLI for interactive debugging
+
+    # Run the CLI
+    CLI(net)
+
+    # After the user exits the CLI, stop the network
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')
-    setupNetwork()
+    create_network()
