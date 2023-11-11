f7b4abde957ec5bdf57f8459af3771994653d285
Nelson Torres <nelsontorresjr330@gmail.com>
2023-11-10 21:03:46 -0600

    more tests

--- assignment2/vm1_topology.py
+++ assignment2/vm1_topology.py
@@ -15,7 +15,7 @@ def create_network():
     # Add three hosts
     h1 = net.addHost('h1', ip='10.0.0.1')
     h2 = net.addHost('h2', ip='10.0.0.2')
-    h3 = net.addHost('h3', ip='10.0.0.3')  # New host
+    h3 = net.addHost('h3', ip='10.100.0.1')
     
     # Add two switches
     s1 = net.addSwitch('s1')
@@ -23,8 +23,8 @@ def create_network():
     
     # Create links between hosts and switches
     net.addLink(h1, s1)
-    net.addLink(h2, s2)
-    net.addLink(h3, s1)  # Link the new host h3 to switch s1
+    net.addLink(h2, s1)
+    net.addLink(h3, s2) 
     
     # Create a link between switches to allow communication between hosts
     net.addLink(s1, s2)
@@ -32,18 +32,9 @@ def create_network():
     # Start the network
     net.start()
     
-    # # Add flow to s1 allowing all traffic to s2
-    # s1.cmd('ovs-ofctl add-flow s1 priority=65535,action=output:2')  # Send to port 2 (s2)
-
-    # # Add flow to s2 allowing all traffic from s1
-    # s2.cmd('ovs-ofctl add-flow s2 priority=65535,action=output:1')  # Send to port 1 (s1)
-
-    # # Add flow to s1 allowing all traffic to s2
-    # s1.cmd('ovs-ofctl add-flow s1 priority=65535,action=normal')
-
-    # # Add flow to s2 dropping ICMP packets from s1 to hosts behind s2
-    # s2.cmd('ovs-ofctl add-flow s2 priority=65535,icmp,nw_src=10.0.0.1,action=drop')
-    # s2.cmd('ovs-ofctl add-flow s2 priority=65535,icmp,nw_src=10.0.0.3,action=drop')
+    # Run the command to block traffic on switch s2
+    s2 = net.get('s2')
+    s2.cmd('ovs-ofctl add-flow s2 priority=100,table=0,ip,nw_src=10.0.0.0/24,actions=drop')
 
     # Test network connectivity
     net.pingAll()
