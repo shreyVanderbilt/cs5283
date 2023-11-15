888e2672863fb6f72501690fe5283b39aeb80406
Nelson Torres <nelsontorresjr330@gmail.com>
2023-11-14 20:43:08 -0600

    test

--- assignment2/vm1_topology.py
+++ assignment2/vm1_topology.py
@@ -1,50 +1,39 @@
-#!/usr/bin/python
-
 from mininet.net import Mininet
 from mininet.node import Controller, OVSKernelSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel
+from mininet.link import Link
 
 def create_network():
-    # Create an instance of the network
+    "Create a network with two switches and four hosts."
+
     net = Mininet(controller=Controller, switch=OVSKernelSwitch)
-    
-    # Add a controller
-    c0 = net.addController('c0')
-    
-    # Add three hosts
-    h1 = net.addHost('h1', ip='10.0.0.1')
-    h2 = net.addHost('h2', ip='10.0.0.2')
-    h3 = net.addHost('h3', ip='100.100.0.1')
-    
-    # Add two switches
+
+    print("*** Creating nodes")
+    h1 = net.addHost('h1', ip='10.0.0.1/24')
+    h2 = net.addHost('h2', ip='10.0.0.2/24')
+    h3 = net.addHost('h3', ip='10.0.1.1/24')
+    h4 = net.addHost('h4', ip='10.0.1.2/24')
     s1 = net.addSwitch('s1')
     s2 = net.addSwitch('s2')
-    
-    # Create links between hosts and switches
+
+    print("*** Creating links")
     net.addLink(h1, s1)
     net.addLink(h2, s1)
-    net.addLink(h3, s2) 
-    
-    # Create a link between switches to allow communication between hosts
-    net.addLink(s1, s2)
-    
-    # Start the network
-    net.start()
-    
-    # Run the command to block traffic on switch s2
-    # s2 = net.get('s2')
-    # s2.cmd('ovs-ofctl add-flow s2 nw_src=10.0.0.0/24,actions=drop')
+    net.addLink(h3, s2)
+    net.addLink(h4, s2)
+    net.addLink(s1, s2)  # Link between switches
 
-    # Test network connectivity
-    net.pingAll()
-    
-    # Drop the user in a CLI so they can try other commands
+    print("*** Starting network")
+    net.build()
+    net.addController('c0')
+
+    print("*** Running CLI")
     CLI(net)
-    
-    # Stop the network
+
+    print("*** Stopping network")
     net.stop()
 
 if __name__ == '__main__':
-    setLogLevel('info')  # Set the log level to info to see more detailed output
+    setLogLevel('info')
     create_network()
